dd73a8db119f94d96a2dcce9a4e10df773268aa9
margotgeorget <margot.georget@etu.umontpellier.fr>
2022-12-02 05:46:23 +0100

    Added charger image

--- src/components/success-dialog.tsx
+++ src/components/success-dialog.tsx
@@ -8,15 +8,73 @@ type SuccessDialogProps = {
 }
 
 export default function SuccessDialog({ cards }: SuccessDialogProps) {
-  const { setIsSuccessDialogVisible } = useContext(GameContext)
+  const { setIsSuccessDialogVisible, currentStep } = useContext(GameContext)
 
   return (
-  <Box padding={3}>
-    <Stack alignItems="center" spacing="20px">
-      <Typography variant="h6">Bravo !</Typography>
-      <Typography>Tu as passé une étape, regarde ton inventaire 😉</Typography>
-      <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
-    </Stack>
-  </Box>
+    <>
+    { currentStep === 1 && 
+      <Box padding={3}>
+      <Stack alignItems="center" spacing="20px">
+        <Typography variant="h6">Bravo !</Typography>
+        <Typography>Super, le coffre est ouvert ! Dedans se trouvent un livre fermé et un mémo. Pense à regarde ton inventaire 😉</Typography>
+        <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
+      </Stack>
+    </Box>
+    }
+    { currentStep === 2 && 
+      <Box padding={3}>
+      <Stack alignItems="center" spacing="20px">
+        <Typography variant="h6">Bravo !</Typography>
+        <Typography>Le livre s’ouvre, une clé était cachée dedans. Pense à regarde ton inventaire 😉</Typography>
+        <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
+      </Stack>
+    </Box>
+    }
+    { currentStep === 3 && 
+      <Box padding={3}>
+      <Stack alignItems="center" spacing="20px">
+        <Typography variant="h6">Bravo !</Typography>
+        <Typography>Le tiroir s’ouvre ! C’est là que maman range son ordinateur ! Pense à regarde ton inventaire 😉</Typography>
+        <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
+      </Stack>
+    </Box>
+    }
+    { currentStep === 4 && 
+      <Box padding={3}>
+      <Stack alignItems="center" spacing="20px">
+        <Typography variant="h6">Bravo !</Typography>
+        <Typography>Super le chargeur est réparé ! L’ordinateur va pouvoir se recharger et s’allumer. Pense à regarde ton inventaire 😉</Typography>
+        <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
+      </Stack>
+    </Box>
+    }
+    { currentStep === 5 && 
+      <Box padding={3}>
+      <Stack alignItems="center" spacing="20px">
+        <Typography variant="h6">Bravo !</Typography>
+        <Typography>Mot de passe correct ! L’ordinateur se déverrouille.</Typography>
+        <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
+      </Stack>
+    </Box>
+    }
+    { currentStep === 6 && 
+      <Box padding={3}>
+      <Stack alignItems="center" spacing="20px">
+        <Typography variant="h6">Bravo !</Typography>
+        <Typography>Vous avez trouvez le fichier, malheureusement il est vérrouillé ! Pense à regarde ton inventaire 😉</Typography>
+        <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
+      </Stack>
+    </Box>
+    }
+    { currentStep === 6 && 
+      <Box padding={3}>
+      <Stack alignItems="center" spacing="20px">
+        <Typography variant="h6">Bravo !</Typography>
+        <Typography>Vous avez décodé le fichier vous pouvez maintenant le lire.</Typography>
+        <Button onClick={() => { setIsSuccessDialogVisible(false) }} variant="contained">Cool !</Button>
+      </Stack>
+    </Box>
+    }
+  </>
   )
 }
